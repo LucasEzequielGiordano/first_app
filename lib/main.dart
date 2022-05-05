1950d12c8bf63d06d554df8637d58b7ca315e952
LucasEzequielGiordano <lucasgiordano2219@gmail.com>
2022-05-05 17:09:58 -0300

    added reviews

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,84 @@
-// funcion principal
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/src/app.dart';
+import 'package:flutter_application_1/src/pages/description_place.dart';
+import 'package:flutter_application_1/src/pages/gradient_back.dart';
+import 'package:flutter_application_1/src/pages/review_list.dart';
 
+void main() => runApp(MyApp());
 
-void main() => runApp(const MyApp());
+// ignore: must_be_immutable
+class MyApp extends StatelessWidget {
+  String descriptionDummy =
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
+  MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        //body: new DescriptionPlace("Bahamas", 4, descriptionDummy),
+        body: Stack(
+          children: <Widget>[
+            ListView(
+              children: <Widget>[
+                DescriptionPlace("Bahamas", 4, descriptionDummy),
+                const ReviewList()
+              ],
+            ),
+            const GradientBack()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
